44a001dfa623cb661fb39c7b5c0841ecb6223653
zicaboom <victormonteirojesus@gmail.com>
2021-06-22 22:47:28 -0300

    Aula03

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express"
+import express, {Request, Response, NextFunction} from "express"
+import "express-async-errors"
 import "reflect-metadata"
 import "./database"
 import { router } from "./routes"
@@ -8,5 +9,17 @@ const PORT = 3333
 
 app.use(express.json())
 app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
+    if(err instanceof Error){
+        return res.status(400).json({
+            erro: err.message  
+        })
+    }
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 app.listen(PORT, ()=>console.log(`Server is running in http://localhost:${PORT}`))
